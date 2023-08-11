2c5237de1b36c01d45eb3bb185f762097665f8cc
Marinela <nelamarinela17@gmail.com>
2023-08-11 23:46:35 +0200

    Add dynamic width and height

--- src/Components/Box.js
+++ src/Components/Box.js
@@ -1,39 +1,53 @@
 import React, { useState } from "react";
 
 const Box = (props) => {
-    const [ boxes, setBoxes] = useState([]);
-    const [ color, setColor] = useState("");
+    const [boxes, setBoxes] = useState([]);
+    const [color, setColor] = useState("");
+    const [width, setWidth] = useState(50); 
+    const [height, setHeight] = useState(50); 
     
     const onSubmitHandler = (e) => {
       e.preventDefault();
   
-      setBoxes(prevBoxes => [...prevBoxes, color]);
+      setBoxes(prevBoxes => [...prevBoxes, {color: color, width: width, height: height}]);
     }
   
     return (
         <>
             <form onSubmit={ onSubmitHandler }>
             <div>
-                <label htmlFor="firstName">Color</label>
+                <label htmlFor="color">Color</label>
                 <input 
                 type="text" 
-                name="color"
+                id="color"
                 onChange={ (e) => setColor(e.target.value) }
+                />
+                <label htmlFor="width">Width</label>
+                <input 
+                type="number" 
+                min="10"
+                id="width"
+                onChange={ (e) => setWidth(e.target.value) }
+                />
+                <label htmlFor="height">Height</label>
+                <input 
+                type="number" 
+                min="10"
+                id="height"
+                onChange={ (e) => setHeight(e.target.value) }
                 />
             </div>
             <button>Add</button>
             </form>
-            { console.log(boxes) }
             {
                 boxes.length > 0 && 
-                boxes.map((color, index) => {
-                    {console.log(index, color)}
+                boxes.map((box, index) => {
                     return (<div key={index} style={{ 
                         display: "inline-block",
                         margin: "10px",
-                        height: "50px", 
-                        width: "50px", 
-                        backgroundColor: color
+                        height: box.height + "px", 
+                        width: box.width + "px", 
+                        backgroundColor: box.color
                         }}>
                     </div>
                     );
